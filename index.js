4259e15a12dda3abfb1941f1c2e604d76ceec81b
comical-tag-7436 <shawtynow2008@gmail.com>
2019-11-10 06:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key){
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
